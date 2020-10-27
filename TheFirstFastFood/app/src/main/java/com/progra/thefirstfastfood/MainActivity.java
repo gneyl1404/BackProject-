ba28ff458f5f1eddb8d6dc1b68f7f58c1e814ba1
susanr17 <72636187+susanr17@users.noreply.github.com>
2020-10-27 16:04:53 -0600

    App Android
    Proyecto para trabajar la aplicación del cliente
    Responsables: Oswaldo y Susan

---
+++ TheFirstFastFood/app/src/main/java/com/progra/thefirstfastfood/MainActivity.java
@@ -0,0 +1,14 @@
+package com.progra.thefirstfastfood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
