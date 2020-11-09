346592a234305a30909cee27c2c9f7d56b20c262
oswam18 <56458387+oswam18@users.noreply.github.com>
2020-11-09 15:07:57 -0600

    jframe pedidos Listo

--- AppMovil/src/App/Login.java
+++ AppMovil/src/App/Login.java
@@ -15,6 +15,7 @@ import javax.swing.JPanel;
  */
 public class Login extends javax.swing.JFrame {
 FondoPanel fondo = new FondoPanel();
+
     /**
      * Creates new form Login
      */
@@ -23,6 +24,13 @@ FondoPanel fondo = new FondoPanel();
         initComponents();
     }
 
+    public String CodigoCliente(){
+        String CodigoCliente = "1000";
+        
+        return CodigoCliente;
+    
+}
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -37,6 +45,11 @@ FondoPanel fondo = new FondoPanel();
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jButton1.setText("Iniciar Sesión");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -57,6 +70,13 @@ FondoPanel fondo = new FondoPanel();
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        
+        
+        
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
--- AppMovil/src/App/Pedidos.java
+++ AppMovil/src/App/Pedidos.java
@@ -5,11 +5,24 @@
  */
 package App;
 
+import Servicio.WebService1;
 import java.awt.Graphics;
 import java.awt.Image;
 import static java.awt.image.ImageObserver.WIDTH;
+import java.io.StringReader;
+import java.util.List;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
+import javax.swing.table.DefaultTableModel;
+
+import org.jdom2.input.SAXBuilder;
+import java.io.StringReader;
+import java.nio.charset.CodingErrorAction;
+import java.util.Calendar;
+import java.util.List;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.input.SAXBuilder;
 
 /**
  *
@@ -17,13 +30,31 @@ import javax.swing.JPanel;
  */
 public class Pedidos extends javax.swing.JFrame {
 FondoPanel fondo = new Pedidos.FondoPanel();
-    /**
+ DefaultTableModel objdtm = new DefaultTableModel();
+ WebService1 servi = new WebService1();
+ Login puente = new Login();
+ public String CodigoCliente = puente.CodigoCliente();
+ 
+ /**
      * Creates new form Pedidos
      */
-    public Pedidos() {
+    public Pedidos( ) {
+        
+       
          this.setContentPane(fondo);
         initComponents();
+        String titulos[] = { "FECHA", "PRODUCTO",  "ESTADO"};
+        objdtm.setColumnIdentifiers(titulos);
+        tabla1.setModel(objdtm);
+         
+        
+        
+          CargarPEDIDOS();
+        
     }
+
+  
+ 
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -34,13 +65,20 @@ FondoPanel fondo = new Pedidos.FondoPanel();
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jScrollPane2 = new javax.swing.JScrollPane();
+        tabla3 = new javax.swing.JTable();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        tabla2 = new javax.swing.JTable();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        tabla1 = new javax.swing.JTable();
+        jLabel4 = new javax.swing.JLabel();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        tabla3.setFont(new java.awt.Font("Tahoma", 0, 3)); // NOI18N
+        tabla3.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null},
                 {null, null, null, null},
@@ -51,9 +89,48 @@ FondoPanel fondo = new Pedidos.FondoPanel();
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
-        jScrollPane1.setViewportView(jTable1);
+        jScrollPane2.setViewportView(tabla3);
 
-        jButton1.setText("jButton1");
+        tabla2.setFont(new java.awt.Font("Tahoma", 0, 3)); // NOI18N
+        tabla2.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(tabla2);
+
+        jLabel3.setText("FINALIZADOS");
+
+        jLabel2.setText("EN PROCESO");
+
+        jLabel1.setText("PENDIENTES");
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jButton1.setText("REGRESAR");
+
+        tabla1.setFont(new java.awt.Font("Tahoma", 0, 10)); // NOI18N
+        tabla1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane3.setViewportView(tabla1);
+
+        jLabel4.setFont(new java.awt.Font("Times New Roman", 1, 18)); // NOI18N
+        jLabel4.setText("PEDIDOS");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -62,21 +139,27 @@ FondoPanel fondo = new Pedidos.FondoPanel();
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(47, 47, 47)
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 207, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(81, 81, 81)
+                        .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 339, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(108, 108, 108)
-                        .addComponent(jButton1)))
-                .addContainerGap(48, Short.MAX_VALUE))
+                        .addGap(202, 202, 202)
+                        .addComponent(jLabel4)))
+                .addContainerGap(80, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 295, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(97, 97, 97))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(94, 94, 94)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 163, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 148, Short.MAX_VALUE)
+                .addGap(78, 78, 78)
+                .addComponent(jLabel4)
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 131, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 347, Short.MAX_VALUE)
                 .addComponent(jButton1)
-                .addGap(38, 38, 38))
+                .addGap(81, 81, 81))
         );
 
         pack();
@@ -119,8 +202,16 @@ FondoPanel fondo = new Pedidos.FondoPanel();
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JTable tabla1;
+    private javax.swing.JTable tabla2;
+    private javax.swing.JTable tabla3;
     // End of variables declaration//GEN-END:variables
 
 class FondoPanel extends JPanel {
@@ -136,5 +227,47 @@ class FondoPanel extends JPanel {
 
         }
     }
+ public void CargarPEDIDOS() {
 
+         
+        SAXBuilder builder = new SAXBuilder();
+
+        String resP = servi.getWebService1Soap().listaDePedidosPorCodigo(CodigoCliente);
+        /*Mostrar informacion multiple en base a un xml*/
+        try {
+            
+            
+           Document documento =  builder.build(new StringReader(resP));
+            /*xml manipulable*/
+            Element rootNodo =  documento.getRootElement();
+            /*lista nodos*/
+            List list = rootNodo.getChildren("pedidos");
+
+            for (int i = 0; i < list.size(); i++) {
+
+                Element nodo = (Element) list.get(i);
+                /*fecha, comentario, puntuacion, etc.*/
+                String[] datos = new String[3];
+               
+                
+                
+                
+               datos[0] = nodo.getChildText("Fecha");
+                datos[1] = nodo.getChildText("Producto");
+             
+                datos[2] = nodo.getChildText("Estado");
+              
+               
+                
+                objdtm.addRow(datos);
+
+            }
+
+        } catch (Exception e) {
+        }
+
+    }
+ 
+   
+ 
 }
